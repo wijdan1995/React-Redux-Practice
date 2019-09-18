c9544c71d25390f57fe87ea37a4a685b8fc0a348
wijdan1995 <wijdan.w.k@outlook.sa>
2019-09-18 14:49:38 +0300

    Single Page App & Nesting Components

---
+++ my-app/src/Components/Wijdan.js
@@ -0,0 +1,21 @@
+import React, { Component } from 'react'
+
+class Wijdan extends Component {
+    render() {
+        return (
+            <div className="wijdan">
+                <div>
+                    Name: Wijdan
+                </div>
+                <div>
+                    Age: 25
+                </div>
+                <div>
+                    Hobbies: Embroidery, Crochet, And Sewing
+                </div>
+            </div>
+        )
+    }
+}
+
+export default Wijdan;

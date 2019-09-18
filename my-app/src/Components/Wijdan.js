f6e5b5a408057b4991c8bfb466972f01a6f4e889
wijdan1995 <wijdan.w.k@outlook.sa>
2019-09-18 17:09:54 +0300

    Conditional Output

--- my-app/src/Components/Wijdan.js
+++ my-app/src/Components/Wijdan.js
@@ -3,27 +3,48 @@ import React from 'react'
 const Wijdan = ({ people }) => {
 
     // const { people } = props
-    const peopleList = people.map(person => {
-        return (
-            <div className="wijdan" key={person.id}>
-                <div>
-                    Name: {person.name}
-                </div>
-                <div>
-                    Age: {person.age}
-                </div>
-                <div>
-                    Hobbies: {person.hobbies}
-                </div>
-                <br />
-            </div>
-        )
-    })
+    // const peopleList = people.map(person => {
+    //     if (person.age > 20) {
+
+
+    //         return (
+    //             <div className="wijdan" key={person.id}>
+    //                 <div>
+    //                     Name: {person.name}
+    //                 </div>
+    //                 <div>
+    //                     Age: {person.age}
+    //                 </div>
+    //                 <div>
+    //                     Hobbies: {person.hobbies}
+    //                 </div>
+    //                 <br />
+    //             </div>
+    //         )
+    //     } else {
+    //         return null
+    //     }
+    // })
+
     return (
         <div className="people-list">
-            {peopleList}
+            {people.map(person => {
+                return person.age > 20 ? (
+                    <div className="wijdan" key={person.id}>
+                        <div>
+                            Name: {person.name}
+                        </div>
+                        <div>
+                            Age: {person.age}
+                        </div>
+                        <div>
+                            Hobbies: {person.hobbies}
+                        </div>
+                        <br />
+                    </div>) : null;
+            })}
         </div>
     )
 }
 
-export default Wijdan;
+export default Wijdan

43391d3bf47347def3162ab55f6c32609d0372f4
wijdan1995 <wijdan.w.k@outlook.sa>
2019-09-18 16:43:21 +0300

    Stateless Components

--- my-app/src/Components/Wijdan.js
+++ my-app/src/Components/Wijdan.js
@@ -1,31 +1,29 @@
-import React, { Component } from 'react'
+import React from 'react'
 
-class Wijdan extends Component {
-    render() {
-        // console.log(this.props)
-        const { people } = this.props
-        const peopleList = people.map(person => {
-            return (
-                <div className="wijdan" key={person.id}>
-                    <div>
-                        Name: {person.name}
-                    </div>
-                    <div>
-                        Age: {person.age}
-                    </div>
-                    <div>
-                        Hobbies: {person.hobbies}
-                    </div>
-                    <br />
-                </div>
-            )
-        })
+const Wijdan = ({ people }) => {
+
+    // const { people } = props
+    const peopleList = people.map(person => {
         return (
-            <div className="people-list">
-                {peopleList}
+            <div className="wijdan" key={person.id}>
+                <div>
+                    Name: {person.name}
+                </div>
+                <div>
+                    Age: {person.age}
+                </div>
+                <div>
+                    Hobbies: {person.hobbies}
+                </div>
+                <br />
             </div>
         )
-    }
+    })
+    return (
+        <div className="people-list">
+            {peopleList}
+        </div>
+    )
 }
 
 export default Wijdan;

b84fd2553d5d9fc20d461ee01fd8d48e5423e6fe
wijdan1995 <wijdan.w.k@outlook.sa>
2019-09-19 18:29:29 +0300

    Lifecycle Methods

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -27,6 +27,13 @@ class App extends Component {
     })
   }
 
+  componentDidMount() {
+    console.log('component mounted')
+  }
+  componentDidUpdate(prevProps, prevState) {
+    console.log('component updated')
+    console.log(prevProps, prevState)
+  }
   render() {
     return (
       <div className="App" >

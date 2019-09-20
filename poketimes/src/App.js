4516781a6cf24129b069fc20e405fb2b6de928d9
wijdan1995 <wijdan.w.k@outlook.sa>
2019-09-20 17:36:11 +0300

    Route Parameters part 1

--- poketimes/src/App.js
+++ poketimes/src/App.js
@@ -3,6 +3,7 @@ import Navbar from './Components/Navbar'
 import About from './Components/About'
 import Contact from './Components/Contact'
 import Home from './Components/Home'
+import Post from './Components/Post'
 
 import { BrowserRouter, Route } from 'react-router-dom'
 
@@ -15,6 +16,7 @@ function App() {
         <Route exact path='/' component={Home} />
         <Route path='/contact' component={Contact} />
         <Route path='/about' component={About} />
+        <Route path="/:post_id" component={Post} />
       </div>
     </BrowserRouter>
   );

f99b0617506120974be2da630c7de08cc39c10f7
wijdan1995 <wijdan.w.k@outlook.sa>
2019-09-20 22:39:13 +0300

    Map Dispatch To Props

--- poketimes/src/reducers/rootReducer.js
+++ poketimes/src/reducers/rootReducer.js
@@ -7,6 +7,16 @@ const initState = {
 }
 
 const rootReducer = (state = initState, action) => {
+    // console.log(action)
+    if (action.type === 'DELETE_POST') {
+        let newPost = state.posts.filter(post => {
+            return action.id !== post.id
+        })
+        return {
+            ...state,
+            posts: newPost
+        }
+    }
     return state
 }
 

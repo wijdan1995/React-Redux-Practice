0fc4a0a4c0a873b8f09dd115bb07b4aef8ee0148
wijdan1995 <wijdan.w.k@outlook.sa>
2019-09-20 21:00:19 +0300

    Mapping State to Props

--- poketimes/src/Components/Home.js
+++ poketimes/src/Components/Home.js
@@ -1,10 +1,11 @@
 import React, { Component } from 'react'
-import axios from 'axios'
+// import axios from 'axios'
 import { Link } from 'react-router-dom'
 import Pokeball from '../pokeball.png'
+import { connect } from 'react-redux'
 
 class Home extends Component {
-    state = {
+    /* state = {
         posts: []
     }
     componentDidMount() {
@@ -15,9 +16,12 @@ class Home extends Component {
                     posts: response.data.slice(0, 10)
                 })
             })
-    }
+    } */
+
     render() {
-        const { posts } = this.state
+        console.log(this.props)
+        const { posts } = this.props
+        // const { posts } = this.state the state in not here anymore using redux we get it from the props
         const postList = posts.length ? (
             posts.map(post => {
                 return (
@@ -44,4 +48,12 @@ class Home extends Component {
     }
 }
 
-export default Home
+// Using Redux
+
+const mapStateToProps = (state) => {
+    return {
+        // get the state from the rootReducer
+        posts: state.posts
+    }
+}
+export default connect(mapStateToProps)(Home)
--- poketimes/src/reducers/rootReducer.js
+++ poketimes/src/reducers/rootReducer.js
@@ -1,5 +1,9 @@
 const initState = {
-    posts: []
+    posts: [
+        { id: 1, title: 'One', body: 'One 1' },
+        { id: 2, title: 'Two', body: 'Two 2' },
+        { id: 3, title: 'Three', body: 'Three 3' },
+    ]
 }
 
 const rootReducer = (state = initState, action) => {
